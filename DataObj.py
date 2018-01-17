ef91c03daa5cba9c217fc7212c512556d29dc24a
Your Name <patirck.heenan@colorado.edu>
2018-01-16 18:35:52 -0700

    added future imports to Dataobj

--- DataObj.py
+++ DataObj.py
@@ -1,3 +1,10 @@
+# force floating point division. Can still use integer with //
+from __future__ import division
+# other good compatibility recquirements for python3
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import numpy as np
 
 from .ProcessSingleWave import WaveObj
