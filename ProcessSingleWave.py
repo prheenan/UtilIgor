dd818da17cf9e61bf8b20743f45fe6a6ef840518
Your Name <patirck.heenan@colorado.edu>
2018-01-17 12:08:08 -0700

    working with IWT python2, but not sure why it is necessary?...

--- ProcessSingleWave.py
+++ ProcessSingleWave.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 # This file is used for importing the common utilities classes.
 import numpy as np
-from .UtilGeneral import GenUtilities  as pGenUtil
+import UtilGeneral.GenUtilities  as pGenUtil
 import os
 
 # given a waverecord, how to access the actual wave sruct
