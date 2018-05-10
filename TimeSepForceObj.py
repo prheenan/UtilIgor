1a2934dd0144ce202a1a38085abad39417c6975b
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-05-10 17:46:57 -0600

    added in velocity setter.

--- TimeSepForceObj.py
+++ TimeSepForceObj.py
@@ -196,6 +196,9 @@ class TimeSepForceObj(object):
     @property
     def Velocity(self):
         return self.LowResData.meta.Velocity
+    @Velocity.setter
+    def Velocity(self,v):
+        self.Meta.Velocity = v
     @property
     def ApproachVelocity(self):
         return self.Meta.ApproachVelocity
